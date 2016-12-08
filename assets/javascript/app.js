baec3a8aaaac1547e1fdd670d80b64a8c6640c58
Alisa Schink <Alisa Schink>
2016-12-08 16:27:12 -0500

    fixed question reset timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ var questions = ["Which movie was released first?", "What is considered to be th
                   "Which if the following did Wes Craven NOT direct?", 
                   "Which iconic horror movie killer went to outer space?", 
                   "Which of the following is NOT based off of a Stephen King Novel?", 
-                  "What was the name of Jamie Lee Curtis’s character in Halloween (1978)?", 
+                  "What was the name of the character Jamie Lee Curtis plays in Halloween (1978)?", 
                   "What Serial Killer is the FBI trying to catch in The Silence of the Lambs (1991)?", 
                   "Which horror movie killer has the highest total body count?"];
 
@@ -23,24 +23,24 @@ var answerOptions = [["A. Friday the 13th", "B. Nightmare on Elm Street","C. Hal
 var answers = ["D", "A", "B", "C", "A", "B", "C", "D", "A", "D"];
 
 var explanations = ["Correct Answer: D. Carrie was released in 1976.", 
-              "Correct Answer: A. While Peeping Tom (1960) is often referred to as the first slasher film, it is also the first movie to put the audience in the killer’s point of view.", 
+              "Correct Answer: A. While Peeping Tom (1960) is often referred to as the first slasher film, it is also the first movie to put the audience in the point of view of the killer.", 
               "Correct Answer: B. Pazuzu is a demon known in Assyrian and Babylonian mythology.", 
               "Correct Answer: C. Deaths (in order) included Steve Orth, Casey Becker, Principal Arthur Himbry, Tatum Riley, Kenny Jones, Stu, and Billy.", 
-              "Correct Answer: A. Rosemary’s Baby (1968) was directed by Roman Polanski.", 
+              "Correct Answer: A. Rosemarys Baby (1968) was directed by Roman Polanski.", 
               "Correct Answer: B. The movie Jason X (2001) takes place on a spaceship in the 25th century.", 
               "Correct Answer: C. Manhunter (1986) was based off of the book Red Dragon, written by Thomas Harris.", 
               "Correct Answer: D. Elizabeth Solly and Kim Hammond were the names of the characters that Jamie Lee Curtis plays in The Fog (1980) and Prom Night (1980) respectively. Judith Myers is the name of the older sister that Michael Myers murders in the beginning of the movie.", 
               "Correct Answer: A. Bill was the serial killer that skinned his victims to make a suit for himself in The Silence of the Lambs. FBI agent Clarice Starling went to Hannibal Lecter for insight on the case in the beginning of the movie.", 
               "Correct Answer: D. The character of Jason Voorhees has the highest body count with 158 total kills throughout his movies."];
 
-//counters for correct, incorrect, and unanswered questions
+//declare variables
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 var time = 30;
 var timeCounter;
 var questionNumber = 0
-var questionCounter;
+var questionTimer;
 
 //button to start the game
 $("#startGame").html("<button>" + "Start the Game!" + "</button>");
@@ -48,6 +48,7 @@ $("#startGame").html("<button>" + "Start the Game!" + "</button>");
 //instructions for the game
 $("#instructions").html("<p>" + "You will have 30 seconds to answer each question. There are 10 questions in total." + "</p>");
 
+//on click function to start game
 $("#startGame").on("click", function() {
       $("#startGame").empty();
       $("#instructions").empty();
@@ -56,14 +57,83 @@ $("#startGame").on("click", function() {
 
     });
 
-//function to start the game
+//game function
 var game = function() {
   
-  // //generate a random number to select question and matching answer
+  
+  //question timer
+  $("#timer").html("<h2>" + time + " " + "Seconds" + "</h2>");
+
+  //Run function to set interval
+  function run() {
+    timeCounter = setInterval(decrement, 1000);
+    };
+
+  //executes timer
+  run();
+
+  //sets interval to automatically reset question
+  function questionSelector(){
+    questionTimer = setInterval(resetQuestion, 5000);
+    };
+
+  //The decrement function.
+  function decrement() {
+
+  //Decrease timer by one.
+  time--;
+
+  //Shows the timer in the #timer tag.
+  $("#timer").html("<h2>" + time + " " + "Seconds" + "</h2>");
+
+
+  //Once time hits zero run the stopTime function.
+  if (time === 0) {
+
+    stopTime();
+
+    //increases unanswered counter if timer runs out
+    unanswered++
+    $("#answerDiv").append("<p>" + explanations[questionNumber] + "</p>");
+
+    //tells the user that time is up.
+    $("#questionDiv").html("<h3>" + "Time's up!" + "</h3>");
+    questionNumber++
+    //resets game questions/answers after 5 seconds
+    questionSelector();
+
+
+    };
+  };
+
+  // function to stop timer
+  function stopTime() {
+
+  //Clears our "counter" interval.
+    clearInterval(timeCounter);
+  };
+
+  //function to stop question timer
+    function stopQuestion() {
+      clearInterval(questionTimer)
+    }
+
+  //reset function to set interval
+  function resetQuestion() {
+    $("#questionDiv").empty();
+    $("#answerDiv").empty();
+    $("#timer").empty();
+    time = 30;
+    stopQuestion();
+    game();
+    };
+
+
+// //generate a random number to select question and matching answer
   // var randomNumber = Math.floor(Math.random()*questions.length);
 
   //shows selected question
-  $("#questionDiv").html("<p>" + questions[questionNumber] + "</p>");
+  $("#questionDiv").html("<h3>" + questions[questionNumber] + "</h3>");
   var correctAnswer = answers[questionNumber];
 
   //show selected answer options
@@ -97,86 +167,27 @@ var game = function() {
       //increases correct counter if buttonValue = correctAnswer
       if (buttonValue === correctAnswer) {
         correct++
+        stopTime();
         //congradulate user if correct answer guessed
-        console.log("that's correct!");
-        console.log(explanations[questionNumber]);
+        $("#questionDiv").html("<h3>" + "That's Correct!" + "</h3>");
+        $("#answerDiv").append("<p>" + explanations[questionNumber] + "</p>");
         questionNumber++
-        setInterval(reset, 3000);
+        //resets game questions/answers after 5 seconds
+        questionSelector();
       } else {
         //increases incorrect counter if userGuess != correctAnswer
         incorrect++
-        console.log("incorrect!");
-        console.log(explanations[questionNumber]);
+        stopTime();
+        $("#questionDiv").html("<h3>" + "Incorrect!" + "</h3>");
+        $("#answerDiv").append("<p>" + explanations[questionNumber] + "</p>");
         questionNumber++
-        setInterval(reset, 3000);
+        //resets game questions/answers after 5 seconds
+        questionSelector();
       };
 
     });
 
-  //question timer
-  $("#timer").html("<h2>" + time + " " + "Seconds" + "</h2>");
-
-  //Run function to set interval
-  function run() {
-    timeCounter = setInterval(decrement, 1000);
-    };
-
-  //The decrement function.
-  function decrement() {
-
-  //Decrease timer by one.
-  time--;
-
-  //Show the number in the #show-number tag.
-  $("#timer").html("<h2>" + time + " " + "Seconds" + "</h2>");
-
-
-  //Once time hits zero run the stop function.
-  if (time === 0) {
-
-    stopTime();
-
-    //increases unanswered counter if timer runs out
-    unanswered++
-    console.log(explanations[questionNumber]);
-
-    //Alert the user that time is up.
-    console.log("Time Up!");
-    questionNumber++
-    setInterval(reset, 3000);
-
-
-    };
-  };
-
-  //The stop function
-  function stopTime() {
-
-  //Clears our "counter" interval.
-    clearInterval(timeCounter);
-  };
-
-  //executes timer
-  run();
-
-//reset function to set interval
-  function reset() {
-    $("#questionDiv").empty();
-    $("#answerDiv").empty();
-    stopTime();
-    $("#timer").empty();
-    time = 30;
-    game();
-    };
-  //resets game questions/answers after 3 seconds
-  // questionCounter = setInterval(reset, 3000);
-
-
-
 //shows correct answer and image when answer given or timer runs out
-
-//automatically selects new question after 5 seconds
-//function to ensure same question is not showed multiple times per game
 
 //shows scores at end of game
 
