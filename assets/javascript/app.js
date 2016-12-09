3e732f1d1d8893725f1d87f70cfcab6ea84c2e39
Alisa Schink <Alisa Schink>
2016-12-09 17:36:35 -0500

    score results fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ var questions = ["Which movie was released first?", "What is considered to be th
                   "Which of the following is NOT based off of a Stephen King Novel?", 
                   "What was the name of the character Jamie Lee Curtis plays in Halloween (1978)?", 
                   "What Serial Killer is the FBI trying to catch in The Silence of the Lambs (1991)?", 
-                  "Which horror movie killer has the highest total body count?"];
+                  "Which horror movie killer has the highest total body count?", ""];
 
 var answerOptions = [["A. Friday the 13th", "B. Nightmare on Elm Street","C. Halloween", "D.  Carrie"], 
                       ["A.  Peeping Tom", "B.  Psycho", "C.  Black Sunday", "D.  Eyes Without a Face"], 
@@ -18,9 +18,9 @@ var answerOptions = [["A. Friday the 13th", "B. Nightmare on Elm Street","C. Hal
                       ["A.  The Shining (1980)", "B.  The Dead Zone (1983)", "C.  Manhunter (1986)", "D.  Thinner (1996)"], 
                       ["A.  Elizabeth Solly", "B. Kim Hammond", "C. Judith Myers", "D.  Laurie Strode"], 
                       ["A.  Buffalo Bill", "B.  The Tooth Fairy", "C. Hannibal Lecter", "D. Red Dragon"], 
-                      ["A.  Michael Myers", "B. Leatherface", "C. Freddy Krueger", "D.  Jason Voorhees"]];
+                      ["A.  Michael Myers", "B. Leatherface", "C. Freddy Krueger", "D.  Jason Voorhees"], ""];
 
-var answers = ["D", "A", "B", "C", "A", "B", "C", "D", "A", "D"];
+var answers = ["D", "A", "B", "C", "A", "B", "C", "D", "A", "D", ""];
 
 var explanations = ["Correct Answer: D. Carrie was released in 1976.", 
               "Correct Answer: A. While Peeping Tom (1960) is often referred to as the first slasher film, it is also the first movie to put the audience in the point of view of the killer.", 
@@ -31,7 +31,7 @@ var explanations = ["Correct Answer: D. Carrie was released in 1976.",
               "Correct Answer: C. Manhunter (1986) was based off of the book Red Dragon, written by Thomas Harris.", 
               "Correct Answer: D. Elizabeth Solly and Kim Hammond were the names of the characters that Jamie Lee Curtis plays in The Fog (1980) and Prom Night (1980) respectively. Judith Myers is the name of the older sister that Michael Myers murders in the beginning of the movie.", 
               "Correct Answer: A. Buffalo Bill was the serial killer that skinned his victims to make a suit for himself in The Silence of the Lambs. FBI agent Clarice Starling went to Hannibal Lecter for insight on the case in the beginning of the movie.", 
-              "Correct Answer: D. The character of Jason Voorhees has the highest body count with 158 total kills throughout his movies."];
+              "Correct Answer: D. The character of Jason Voorhees has the highest body count with 158 total kills throughout his movies.", ""];
 
 //images for when answer is shown
 var img1 = $("<img>").attr("src", "assets/images/carrie.jpg");
@@ -45,7 +45,7 @@ var img8 = $("<img>").attr("src", "assets/images/laurie.jpg");
 var img9 = $("<img>").attr("src", "assets/images/buffalo_bill.jpg");
 var img10 = $("<img>").attr("src", "assets/images/jason.jpg");
 
-var images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
+var images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, ""];
 
 //declare variables
 var correct = 0;
@@ -80,7 +80,7 @@ var game = function() {
   //question timer display
   $("#timer").html("<h3>" + time + " " + "Seconds" + "</h3>");
 
-  //Run function to set interval
+  //Run function to set timer interval
   function runTimer() {
     timeCounter = setInterval(decrement, 1000);
     };
@@ -96,29 +96,29 @@ var game = function() {
   //The decrement function.
   function decrement() {
 
-  //Decrease timer by one.
-  time--;
+    //Decrease timer by one.
+    time--;
 
-  //Shows the timer in the #timer tag.
-  $("#timer").html("<h3>" + time + " " + "Seconds" + "</h3>");
+    //Shows the timer in the #timer tag.
+    $("#timer").html("<h3>" + time + " " + "Seconds" + "</h3>");
 
 
-  //Once time hits zero run the stopTime function.
-  if (time === 0) {
+    //Once time hits zero run the stopTime function.
+    if (time === 0) {
 
-    stopTime();
+      stopTime();
 
-    //increases unanswered counter if timer runs out
-    unanswered++
-    //shows correct answer
-    $("#answerDiv").append("<p>" + explanations[questionNumber] + "</p>");
-    //show answer image
-    $("#answerDiv").append(images[questionNumber]);
+      //increases unanswered counter if timer runs out
+      unanswered++
+      //shows correct answer
+      $("#answerDiv").append("<p>" + explanations[questionNumber] + "</p>");
+      //show answer image
+      $("#answerDiv").append(images[questionNumber]);
 
-    //tells the user that time is up.
-    $("#questionDiv").html("<h2>" + "Time's up!" + "</h2>");
-    //resets game questions/answers after 5 seconds
-    questionSelector();
+      //tells the user that time is up.
+      $("#questionDiv").html("<h2>" + "Time's up!" + "</h2>");
+      //resets game questions/answers after 5 seconds
+      questionSelector();
     };
   };
 
@@ -126,14 +126,14 @@ var game = function() {
   // function to stop timer
   function stopTime() {
 
-  //clears "timeCounter" interval.
+    //clears "timeCounter" interval.
     clearInterval(timeCounter);
   };
 
   //clears question timer
-    function stopQuestion() {
-      clearInterval(questionTimer)
-    }
+  function stopQuestion() {
+    clearInterval(questionTimer)
+  }
 
   //resets question
   function resetQuestion() {
@@ -144,27 +144,27 @@ var game = function() {
     time = 30;
     questionNumber++
     game();
-    };
+  };
 
   //shows scores at end of game
-    if (questionNumber > 9){
-      stopQuestion();
-      stopTime();
-      $("#timer").empty();
-      $("#questionDiv").empty();
-      $("#answerDiv").empty();
-      $("#results").append("<h2>" + "Game Over!" + "</h2>");
-      $("#results").append("<h4>" + "Correct Answers: " + correct + "</h4");
-      $("#results").append("<h4>" + "Inorrect Answers: " + incorrect + "</h4>");
-      $("#results").append("<h4>" + "Unanswered Questions: " + unanswered + "</h4>");
-      $("#results").append("<h2>" + "Total Score: " + (correct/10)*100 + "%" + "</h2>");
+  if (questionNumber > 9){
+    stopQuestion();
+    stopTime();
+    $("#timer").empty();
+    $("#questionDiv").empty();
+    $("#answerDiv").empty();
+    $("#results").append("<h2>" + "Game Over!" + "</h2>");
+    $("#results").append("<h4>" + "Correct Answers: " + correct + "</h4>");
+    $("#results").append("<h4>" + "Inorrect Answers: " + incorrect + "</h4>");
+    $("#results").append("<h4>" + "Unanswered Questions: " + unanswered + "</h4>");
+    $("#results").append("<h2>" + "Total Score: " + (correct/10)*100 + "%" + "</h2>");
       
-      //game reset button
-      var resetButton = $("<div>").addClass("gameButton");
-        resetButton.html("<h4>" + "Try Again!" + "</h4>");
-        $("#resetGame").append(resetButton);
+    //game reset button
+    var resetButton = $("<div>").addClass("gameButton");
+    resetButton.html("<h4>" + "Try Again!" + "</h4>");
+    $("#resetGame").append(resetButton);
 
-    }; 
+  }; 
 
 
   //shows selected question
@@ -172,26 +172,30 @@ var game = function() {
   var correctAnswer = answers[questionNumber];
 
   //show selected answer options
-   //creates buttons for selected answer options
+  //declares answerArray variable
   var answerArray = answerOptions[questionNumber];
  
-  var A = $("<div>").addClass("answerButton");
-  var B = $("<div>").addClass("answerButton");
-  var C = $("<div>").addClass("answerButton");
-  var D = $("<div>").addClass("answerButton");
+  //creates buttons until all questions have been cycled
+  if (questionNumber < 10){
+    //creates buttons for selected answer options
+    var A = $("<div>").addClass("answerButton");
+    var B = $("<div>").addClass("answerButton");
+    var C = $("<div>").addClass("answerButton");
+    var D = $("<div>").addClass("answerButton");
 
-  var buttons = [A, B, C, D];
-  var letters = ["A", "B", "C", "D"];
+    var buttons = [A, B, C, D];
+    var letters = ["A", "B", "C", "D"];
   
-  //for loop to create buttons for each multiple choice answer.
-  for (var i = 0; i < 4; i++) {
+    //for loop to create buttons for each multiple choice answer.
+    for (var i = 0; i < 4; i++) {
 
-    var options = $(buttons[i]).attr("data-value", letters[i]).html("<h4>" + answerArray[i] + "</h4>");
-    //each answer option will get added to the page.
-
-    $("#answerDiv").append(options);
+      var options = $(buttons[i]).attr("data-value", letters[i]).html("<h4>" + answerArray[i] + "</h4>");
+    
+      //each answer option will get added to the page.
+      $("#answerDiv").append(options);
      
-  };
+    };
+  };  
 
   //onclick function for userGuess
 
@@ -200,6 +204,7 @@ var game = function() {
     var buttonValue = ($(this).data("value"));
 
       console.log(buttonValue);
+
       //increases correct counter if buttonValue = correctAnswer
       if (buttonValue === correctAnswer) {
         //increases the correct counter by 1
@@ -210,12 +215,12 @@ var game = function() {
         $("#questionDiv").html("<h2>" + "That's Correct!" + "</h2>");
         //shows correct answer
         $("#answerDiv").append("<p>" + explanations[questionNumber] + "</p>");
-        //show answer image
+        //shows answer image
         $("#answerDiv").append(images[questionNumber]);
         //resets game questions/answers after 5 seconds
         questionSelector();
       } else {
-        //increases incorrect counter if userGuess does not =  correctAnswer
+        //increases incorrect counter if userGuess does not = correctAnswer
         incorrect++
         //stops the timer
         stopTime();
@@ -223,7 +228,7 @@ var game = function() {
         $("#questionDiv").html("<h2>" + "Incorrect!" + "</h2>");
         //shows correct answer
         $("#answerDiv").append("<p>" + explanations[questionNumber] + "</p>");
-        //show answer image
+        //shows answer image
         $("#answerDiv").append(images[questionNumber]);
         //resets game questions/answers after 5 seconds
         questionSelector();
